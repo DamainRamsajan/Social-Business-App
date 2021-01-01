d2d89c7536608e04b490587b3770b6e94336e53b
Damain Ramsajan <dpramsajan@gmail.com>
2021-01-01 23:19:14 +0100

    app refactored with redux template up to login complete

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARZz1L5g7HKTmvrnxdnjoTsfa7iL87wPQ",
+  authDomain: "redux-linkedin-clone.firebaseapp.com",
+  projectId: "redux-linkedin-clone",
+  storageBucket: "redux-linkedin-clone.appspot.com",
+  messagingSenderId: "44909773140",
+  appId: "1:44909773140:web:e25ca7774bcfb029e188c1"
+};
+
+  const firebaseApp = firebase.initializeApp (firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
+
+  
